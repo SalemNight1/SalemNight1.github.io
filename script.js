47d3e23065fe2d57cf78e68991beede1763c0b8e
Artour Grigorian <artur.0107@mail.ru>
2022-12-23 20:35:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+$('.nav-menu-btn').on('click', function() {
+  $('.navigation').addClass('navigation_active');
+})
+
+$('.nav-close-btn').on('click', function() {
+  $('.navigation').removeClass('navigation_active');
+})
